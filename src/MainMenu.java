4d04fdbf3bba8f612e9a261c00050174f327c5ff
Zeus-32 <marekkubik06@seznam.cz>
2024-05-23 15:47:47 +0200

    2

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -35,6 +35,7 @@ public class MainMenu extends JPanel implements ActionListener {
         // Nastavení pozic tlačítek
         spustitButton.setBounds(buttonX, buttonY, buttonWidth, buttonHeight);
         odejitButton.setBounds(buttonX, buttonY + buttonHeight + buttonSpacing, buttonWidth, buttonHeight);
+        
         add(spustitButton);
         add(odejitButton);
     }
